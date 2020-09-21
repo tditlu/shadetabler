e248f9a86ba424bd4dc18f44f992afd9e69658b2
Daniel Gerdgren <daniel@oldapes.com>
2020-09-21 22:33:33 +0200

    Update version

--- src/shadetabler.c
+++ src/shadetabler.c
@@ -10,7 +10,7 @@
 #include "exoquant/exoquant.h"
 #include "cwalk/cwalk.h"
 
-#define SHADETABLER_VERSION "1.0.1"
+#define SHADETABLER_VERSION "1.0.2"
 #define SHADETABLER_VERSION_DATE "2020-09-21"
 #define SHADETABLER_HEADER "ShadeTabler (Shade Table Generator) by Todi / Tulou - version " SHADETABLER_VERSION " (" SHADETABLER_VERSION_DATE ")"
 
